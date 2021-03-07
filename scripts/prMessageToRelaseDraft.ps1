b7809ba3124126df6322d85ab272f1bf3324b4c1
StefanIvemo <stefan@ivemo.se>
2021-03-07 13:32:27 +0100

    WIP

--- scripts/prMessageToRelaseDraft.ps1
+++ scripts/prMessageToRelaseDraft.ps1
@@ -8,36 +8,42 @@ param (
 
 $Header = @{
     "authorization" = "token $Token"
-    "Accept" = "application/vnd.github.v3+json"
+    "Accept"        = "application/vnd.github.v3+json"
 }
 
 #Get all releases
 $getReleases = Invoke-RestMethod -Method Get -Headers $Header -URI  "https://api.github.com/repos/StefanIvemo/ActionsTest/releases"
 
 #Check if a release draft exists
-foreach ($release in $getReleases){
+foreach ($release in $getReleases) {
     if ($release.draft -and ($release.tag_name -eq "vNext")) {
         Write-Host "Found a draft with id $($release.id)"
-        $draftID=$release.id
-        $draftBody=$release.body
+        $draftID = $release.id
+        $draftBody = $release.body
     }
 }
 
 #Add merged PR to release notes draft
 if (-not [string]::IsNullOrWhiteSpace($draftBody)) {
     $draftBody += "$PRTitle (#$PRNumber)"
-} else {
+}
+else {
     $draftBody = "$PRTitle (#$PRNumber)"
 }
 
+if (!$draftID) {
 
-
-#Create new draft
-$Body = @{
-    tag_name    = "vNext"
-    name        = "WIP - Next Release"
-    body        = $draftBody
-    draft       = $true
+    #Create new draft
+    $Body = @{
+        tag_name = "vNext"
+        name     = "WIP - Next Release"
+        body     = $draftBody
+        draft    = $true
+    }
+    $requestBody = ConvertTo-Json $Body
+    $createReleaseDraft = Invoke-RestMethod -Method Post -Headers $Header -Body $requestBody -URI  "https://api.github.com/repos/StefanIvemo/ActionsTest/releases" -Verbose
+} else {
+    #https://docs.github.com/en/rest/reference/repos#update-a-release
 }
-$requestBody=ConvertTo-Json $Body
-$createReleaseDraft = Invoke-RestMethod -Method Post -Headers $Header -Body $requestBody -URI  "https://api.github.com/repos/StefanIvemo/ActionsTest/releases" -Verbose
+
+
